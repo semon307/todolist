5f081e70a8c18daf48074d67f248766bf318b639
Semen Kozhin <sa.kozh@yandex.ru>
2021-12-21 09:42:12 +0100

    Uploaded project, 21.12.21

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import './App.css'
+import { TodolistsList } from './UI/TodolistsList/TodolistsList'
+import {useDispatch, useSelector} from 'react-redux'
+import { AppRootStateType } from './BLL/store'
+import {authMeTC, RequestStatusType} from './BLL/app-reducer'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ErrorSnackbar } from './UI/Common/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "./UI/Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {CircularProgress} from "@mui/material";
+import {logoutTC} from "./BLL/AuthReducer";
+
+
+
+type PropsType = {
+    demo?: boolean
 }
 
-export default App;
+function App({demo = false}: PropsType) {
+    const dispatch = useDispatch()
+    useEffect(()=> {
+        dispatch(authMeTC())
+    }, [])
+    const logoutHandler = () => {
+        dispatch(logoutTC())
+    }
+    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+    if (!isInitialized){
+        return  <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+
+    }
+    return (
+        <div className="App">
+            <ErrorSnackbar/>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    {isLoggedIn ? <Button color="inherit" onClick={logoutHandler}>Logout</Button> : null}
+                </Toolbar>
+                {status === 'loading' && <LinearProgress/>}
+            </AppBar>
+            <Container fixed>
+                <Routes>
+                    <Route path={"/"} element={<TodolistsList demo={demo}/>}/>
+                    <Route path={"login"} element={<Login/>}/>
+                    <Route path="404" element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path="*" element={<Navigate to='/404'/>}/>
+                </Routes>
+                {/*<TodolistsList demo={demo}/>*/}
+                {/*<Login/>*/}
+            </Container>
+        </div>
+    )
+}
+
+export default App
---
+++ src/BLL/AuthReducer.ts
@@ -0,0 +1,45 @@
+import { Dispatch } from 'redux'
+import { SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType } from './app-reducer'
+import {authAPI, LoginParamsType} from "../DAL/todolists-api";
+import {handleServerAppError, handleServerNetworkError} from "../utils/error-utils";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+
+const initialState = {
+    isLoggedIn: false
+}
+const slice = createSlice({
+    name: "auth",
+    initialState: initialState,
+    reducers: {
+        setIsLoggedInAC(state, action: PayloadAction<{value: boolean}>){
+            state.isLoggedIn = action.payload.value
+        }
+    }
+})
+export const authReducer = slice.reducer;
+export const setIsLoggedInAC = slice.actions.setIsLoggedInAC
+
+
+export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
+    dispatch(setAppStatusAC({status: 'loading'}))
+    authAPI.login(data)
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC({value: true}))
+                dispatch(setAppStatusAC({status: "succeeded"}))
+            } else {
+                handleServerAppError(res.data, dispatch);
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+export const logoutTC = () => (dispatch: Dispatch) => {
+    authAPI.logout().then(res => {
+        if (res.data.resultCode === 0){
+            dispatch(setIsLoggedInAC({value: false}))
+        }
+    })
+}
